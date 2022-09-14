f832d5dfcffa6d872fbaa16a75cc03f23c7b3d40
qoqbobb <kail853@naver.com>
2022-09-14 19:28:50 +0900

    asdasdasdasd

--- src/main/java/com/springboot/project/controller/ReplyController.java
+++ src/main/java/com/springboot/project/controller/ReplyController.java
@@ -1,16 +1,10 @@
 package com.springboot.project.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.project.model.ReplyVO;
