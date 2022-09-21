406fe72253a78482c7db8ff670a78ce905f18a75
minhyuck Kim <rhkddka02@naver.com>
2022-09-21 11:36:34 +0900

    pd서비스 임플 수정

--- src/main/java/com/springboot/project/sevice/PdService.java
+++ src/main/java/com/springboot/project/sevice/PdService.java
@@ -2,9 +2,16 @@ package com.springboot.project.sevice;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.springboot.project.mapper.Pdmapper;
+import com.springboot.project.model.FileVO;
 import com.springboot.project.model.Pdboard;
 
+@Service("com.springboot.project.service.PdService")
 public interface PdService {
+
+	public static final Pdmapper mPdMapper = null;
 	
 	 List<Pdboard> getpdBoardList(Pdboard pdboard);
 
@@ -16,4 +23,13 @@ public interface PdService {
 
 	    void deletepdBoard(Pdboard pdboard);
 	
+	    public static int fileInsertService(FileVO file) throws Exception{
+	    	return mPdMapper.fileInsert(file);
+	    }
+	    
+	    
 }
+
+	
+
+
